dc564ae4996244a2ef939ad341098a0f28b6b1e3
Your Name <nguyenducanhtuan0602@gmail.com>
2024-05-30 15:06:42 +0700

    update getStudent

--- src/main/java/com/example/AssignmentSing/Controller/WelcomeController.java
+++ src/main/java/com/example/AssignmentSing/Controller/WelcomeController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1")
 public class WelcomeController {
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String welcome() {
         return "hello world";
     }
